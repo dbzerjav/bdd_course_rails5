06fb053ce77e3129950e11a05d32425da41388f7
dbzerjav <dbzerjav@gmail.com>
2017-03-10 18:47:12 +0000

    imlement display of message when no articles present

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Listing articles" do
   end
   
   
-  scenario "A user lists all articles" do
+  scenario "A user has no articles" do
     visit "/"
     
     expect(page).to have_content(@article1.title)
@@ -21,4 +21,20 @@ RSpec.feature "Listing articles" do
     
   end
   
+  scenario "A user lists all articles" do
+    Article.delete_all
+    visit "/"
+    
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+    
+    within("h1#no-articles") do
+      expect(page).to have_content("No articles created")
+    end
+    
+  end
 end
