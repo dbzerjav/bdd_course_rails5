bbb87b599982a3d9e8b324ab15c56a1f376a62a8
dbzerjav <dbzerjav@gmail.com>
2017-03-13 18:59:56 +0000

    show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,14 +3,41 @@ require "rails_helper"
 RSpec.feature "Listing articles" do
   
   before do 
-    john = User.create(email: "john@example.com", password: "password" )
+    @john = User.create(email: "john@example.com", password: "password" )
     
-    @article1 = Article.create(title: "The first article", body: "Lorem ipsum batman", user: john)
-    @article2 = Article.create(title: "The second article", body: "body of second article", user: john)
+    @article1 = Article.create(title: "The first article", body: "Lorem ipsum batman", user: @john)
+    @article2 = Article.create(title: "The second article", body: "body of second article", user: @john)
+  end
+  
+    scenario "with articles created and user not signed in" do
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+    
+  end
+  
+  scenario "with articles created and user signed in" do
+    login_as(@john)
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+    
   end
   
   
-  scenario "A user has no articles" do
+  scenario "A user lists all articles" do
     visit "/"
     
     expect(page).to have_content(@article1.title)
@@ -22,7 +49,7 @@ RSpec.feature "Listing articles" do
     
   end
   
-  scenario "A user lists all articles" do
+  scenario "A user has no articles" do
     Article.delete_all
     visit "/"
     
